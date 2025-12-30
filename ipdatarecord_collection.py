69b71ce6e01fa7464fbe4583c627f072dfdd7eb6
Swarnya Srivastava <49378967+swarnya246@users.noreply.github.com>
2025-12-30 10:23:02 -0800

    Add files via upload

--- ipdatarecord_collection.py
+++ ipdatarecord_collection.py
@@ -29,15 +29,96 @@ def save_csv(data, timestamp):
     print(f"CSV saved: {filename}")
     return df
 
+def save_charts(df, plot_title, file_title, timestamp):
+    """Save specific charts"""
+    plt.figure(figsize=(10, 6))
+    plt.plot(df['hour'], df['mean'])
+    plt.xlabel('Start Time')
+    plt.ylabel('Transmitted and Recieved Megabytes')
+    plt.title(f'Average Usage by Hour of {plot_title}')
+    plt.xticks(range(0, 24), [f'{h:02d}:00' for h in range(24)], rotation=45, ha='right')
+    plt.grid(False)
+    plt.tight_layout()
+    plt.savefig(f'{OUTPUT_DIR}/tx_rx_cumulative_avg_{file_title}_{timestamp}.png', dpi=300)
+    plt.close()
+    
+    plt.figure(figsize=(10, 6))
+    plt.bar(df['hour'], df['sum'])
+    plt.xlabel('Start Time')
+    plt.ylabel('Transmitted and Recieved Megabytes')
+    plt.title(f'Sum of Usage by Hour of {plot_title}')
+    plt.xticks(range(0, 24), [f'{h:02d}:00' for h in range(24)], rotation=45, ha='right')
+    plt.grid(False)
+    plt.tight_layout()
+    plt.savefig(f'{OUTPUT_DIR}/tx_rx_cumulative_sum_{file_title}_{timestamp}.png', dpi=300)
+    plt.close()
+
 def generate_charts(df, timestamp):
     """Generate visualizations"""
+
+    # Separate school year and summer break and weekdays from weekends
+    df_school_year = df.loc[((df['start_time'] >= '2024-9-03') & (df['start_time'] < '2025-6-18'))
+                     | (df['start_time'] >= '2025-9-03')]
+    df_summer_break = df.loc[(df['start_time'] < '2024-9-03') |
+                 ((df['start_time'] >= '2025-6-18')
+                 & (df['start_time'] < '2025-9-03'))]
+    
+    df_school_weekdays = df_school_year.loc[df_school_year['start_time'].dt.dayofweek < 5]
+    df_school_weekdays['hour'] = df_school_weekdays['start_time'].dt.hour
+    school_weekday_hourly = df_school_weekdays.groupby('hour')['tx_and_rx_mb'].agg(['mean', 'sum', 'count', 'std'])
+    school_weekday_hourly = school_weekday_hourly.reset_index()
+    save_charts(school_weekday_hourly, "School Weekday", "school_weekday", timestamp)
+    
+    df_school_weekends = df_school_year.loc[df_school_year['start_time'].dt.dayofweek >= 5]
+    df_school_weekends['hour'] = df_school_weekends['start_time'].dt.hour
+    school_weekends_hourly = df_school_weekends.groupby('hour')['tx_and_rx_mb'].agg(['mean', 'sum', 'count', 'std'])
+    school_weekends_hourly = school_weekends_hourly.reset_index()
+    save_charts(school_weekends_hourly, "School Weekend", "school_weekend", timestamp)
+
+    df_summer_weekdays = df_summer_break.loc[df_summer_break['start_time'].dt.dayofweek < 5]
+    df_summer_weekdays['hour'] = df_summer_weekdays['start_time'].dt.hour
+    summer_weekday_hourly = df_summer_weekdays.groupby('hour')['tx_and_rx_mb'].agg(['mean', 'sum', 'count', 'std'])
+    summer_weekday_hourly = summer_weekday_hourly.reset_index()
+    save_charts(summer_weekday_hourly, "Summer Weekday", "summer_weekday", timestamp)
+
+    df_summer_weekends = df_summer_break.loc[df_summer_break['start_time'].dt.dayofweek >= 5]
+    df_summer_weekends['hour'] = df_summer_weekends['start_time'].dt.hour
+    summer_weekends_hourly = df_summer_weekends.groupby('hour')['tx_and_rx_mb'].agg(['mean', 'sum', 'count', 'std'])
+    summer_weekends_hourly = summer_weekends_hourly.reset_index()
+    save_charts(summer_weekends_hourly, "Summer Weekend", "summer_weekend", timestamp)
+
+    # Extract last month's summary
+    today = datetime.now()
+    first_day_current_month = today.replace(day=1)
+    first_day_last_month = (first_day_current_month - pd.Timedelta(days=1)).replace(day=1)
+    df = df.loc[(df['start_time'] >= first_day_last_month.strftime('%Y-%m-%d'))
+                & (df['start_time'] < first_day_current_month.strftime('%Y-%m-%d'))]
+    
     plt.figure(figsize=(10, 6))
     plt.scatter(df['start_time'], df['tx_and_rx_mb'], alpha=0.5, s=6)
     plt.xlabel('Start Time')
     plt.ylabel('Transmitted and Recieved Megabytes')
     plt.title(f'Monthly Summary: {timestamp}')
     plt.tight_layout()
-    plt.savefig(f'{OUTPUT_DIR}/summary_{timestamp}.png', dpi=300)
+    plt.savefig(f'{OUTPUT_DIR}/tx_rx_summary_{timestamp}.png', dpi=300)
+    plt.close()
+
+    plt.figure(figsize=(10, 6))
+    plt.scatter(df['start_time'], df['tx_mb'], alpha=0.5, s=6)
+    plt.xlabel('Start Time')
+    plt.ylabel('Transmitted Megabytes')
+    plt.title(f'Monthly Summary: {timestamp}')
+    plt.tight_layout()
+    plt.savefig(f'{OUTPUT_DIR}/tx_summary_{timestamp}.png', dpi=300)
+    plt.close()
+
+    plt.figure(figsize=(10, 6))
+    plt.scatter(df['start_time'], df['rx_mb'], alpha=0.5, s=6)
+    plt.xlabel('Start Time')
+    plt.ylabel('Recieved Megabytes')
+    plt.title(f'Monthly Summary: {timestamp}')
+    plt.tight_layout()
+    plt.savefig(f'{OUTPUT_DIR}/rx_summary_{timestamp}.png', dpi=300)
     plt.close()
     
     print("Charts generated")
@@ -54,13 +135,8 @@ def main():
         df = save_csv(data, timestamp)
         df['start_time'] = pd.to_datetime(df['start_time'])
         df['tx_and_rx_mb'] = (df['tx_total_packets'] + df['rx_total_packets']) * 1500/1000000
-
-        # Extract last month
-        today = datetime.now()
-        first_day_current_month = today.replace(day=1)
-        first_day_last_month = (first_day_current_month - pd.Timedelta(days=1)).replace(day=1)
-        df = df.loc[(df['start_time'] >= first_day_last_month.strftime('%Y-%m-%d'))
-                    & (df['start_time'] < first_day_current_month.strftime('%Y-%m-%d'))]
+        df['tx_mb'] = df['tx_total_packets'] * 1500/1000000
+        df['rx_mb'] = df['rx_total_packets'] * 1500/1000000        
         
         # Generate charts
         generate_charts(df, timestamp)
